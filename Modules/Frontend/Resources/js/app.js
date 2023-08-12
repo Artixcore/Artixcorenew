6e6165ab2a528dec6c1ca2336a99b40ac9b85af9
IsmamSoft <ismamsoft@gmail.com>
2023-08-12 12:55:28 +0600

    Module Deleted

--- Modules/Frontend/Resources/js/app.js
+++
@@ -1,6 +0,0 @@
-// import './bootstrap';
-import { createApp } from 'vue';
-import App from './components/App.vue';
-import router from './router/router.js';
- 
-createApp(App).use(router).mount("#app")
