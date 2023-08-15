b32fc8d4c4656d9837a1e0f19060687fcbe8a3ca
IsmamSoft <ismamsoft@gmail.com>
2023-08-15 13:26:24 +0600

    update Frontend

--- resources/js/components/frontend/Header/Nav.vue
+++ resources/js/components/frontend/Header/Nav.vue
@@ -58,9 +58,16 @@
                 </div>
             </div>
         </div>
-</header></template>
+</header>
+</template>
   
-<style>@import "./assets/css/plugins.css";
-@import "./assets/search/search.css";
-@import "./assets/quform/css/base.css";
-@import "./assets/css/styles.css";</style>
+<style>
+@import 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i';
+@import 'assets/vendor/aos/aos.css';
+@import 'assets/vendor/bootstrap/css/bootstrap.min.css';
+@import 'assets/vendor/bootstrap-icons/bootstrap-icons.css';
+@import 'assets/vendor/glightbox/css/glightbox.min.css';
+@import 'assets/vendor/remixicon/remixicon.css';
+@import 'assets/vendor/swiper/swiper-bundle.min.css';
+@import 'assets/css/style.css';
+</style> 
--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -14,7 +14,7 @@ const routes = [
         path: "/Login",
         component: Login,
     },
-    {
+    { 
         path: "/Register",
         component: Register,
     },
